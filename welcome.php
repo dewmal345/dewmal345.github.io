0ffcf98724f5e8a8d01741d1e9ed372e48006d5b
dewmal345 <dewmal.anicitus@gmail.com>
2014-03-31 15:43:49 +0530

    ss
    sss

--- welcome.php
+++ welcome.php
@@ -5,7 +5,7 @@ $email=  $_POST["email"];
 $file = fopen("email.txt","w");
  fwrite($file,$email);
  $from="dewmal.anicitus@gmail.com";
- mail("dewmal.anicitus@gmail.com","test","test","From: $from\n");
+ 
 fclose($file);
 
  ?>
